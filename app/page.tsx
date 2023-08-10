49fc1e3bd55fa89e5575dc0475be42d654d2761c
Carolyn Zhang <hi@carolynzhang.com>
2023-08-10 12:02:46 -0400

    basic chat -- sometimes it doesn't work tho?

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,102 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
+
+let socket: Socket; //i hope it will be happy with this
+
+type Message = {
+  author: string;
+  message: string;
+};
 
 export default function Home() {
+  /* functionality!
+/ 1. global pomodoro time:
+  - see how many people are online
+  - everyone is on same clock
+    - 12:00 - 12:25 you can't talk to anyone
+    - 12:25 - 12:30 you can chat in global chat
+    - 12:30 - 12:55 you can't talk to anyone
+    - 12:55 - 1:00 you can chat in global chat
+  2. chat functionality
+    - normal global chat
+    - enter name in order to chat, but you don't need to enter name in order to use the pomodoro timer
+      - TBD, MIGHT CHANGE ORDER OF NAME-ASKING  
+    - not stored after the 5 minute mark is up
+    
+*/
+  const [chatOpen, setChatOpen] = useState(false);
+  const [name, setName] = useState("default");
+  const [messages, setMessages] = useState<Array<Message>>([]);
+  const [myMessage, setMyMessage] = useState("");
+  const [numberOnline, setNumberOnline] = useState(0);
+  const [countdown, setCountdown] = useState(0); //  figure out what number / way to store this — we will think about it later :))))
+
+  // set up socket connection
+  useEffect(() => {
+    socketInitializer();
+  }, []);
+
+  const socketInitializer = () => {
+    // We just call it because we don't need anything else out of it
+    //await fetch("/api/socket");
+
+    socket = io("https://pomodoro-server-o0a9.onrender.com");
+
+    socket.on("newIncomingMessage", (msg) => {
+      console.log("incoming message: ", msg);
+      setMessages((currentMsg) => [
+        ...currentMsg,
+        { author: msg.author, message: msg.message },
+      ]);
+      console.log(messages);
+    });
+  };
+
+  const sendMessage = async () => {
+    socket.emit("createdMessage", { author: name, message: myMessage });
+    setMessages((currentMsg) => [
+      ...currentMsg,
+      { author: name, message: myMessage },
+    ]);
+    setMyMessage("");
+  };
+
+  const handleKeypress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      if (myMessage) {
+        sendMessage();
+      }
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div> hola! </div>
+      <div className="flex flex-col justify-end bg-black text-green-400 h-[40rem] min-w-[90%] shadow-md ">
+        <div className="h-full last:border-b-0 overflow-y-scroll">
+          {messages.map((msg, i) => {
+            return (
+              <div className="w-full py-1 px-2" key={i}>
+                {msg.author} : {msg.message}
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-full flex">
+          <input
+            type="text"
+            placeholder="New message..."
+            value={myMessage}
+            className="outline-none py-2 px-2 rounded-bl-md flex-1 bg-black text-green-400"
+            onChange={(e) => setMyMessage(e.target.value)}
+            onKeyUp={handleKeypress}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
