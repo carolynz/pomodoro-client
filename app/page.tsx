e2a322823f725620c65f8161c18399a444116b20
Carolyn Zhang <hi@carolynzhang.com>
2023-08-15 18:06:56 -0400

    initial styling, nott super responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-let socket: Socket; //i hope it will be happy with this
+// do we need this line?
+let socket: Socket;
 
 type Message = {
   author: string;
@@ -64,7 +65,9 @@ export default function Home() {
 
   const minutes = Math.floor(countdown / (60 * 1000));
   const seconds = Math.floor((countdown % (60 * 1000)) / 1000);
-  const secondsPadded = String(seconds).padStart(2, "0"); // This pads the seconds with a leading zero if necessary
+  // pad the min/sec with a leading zero if necessary
+  const minutesPadded = String(minutes).padStart(2, "0");
+  const secondsPadded = String(seconds).padStart(2, "0");
 
   // CHAT + WEBSOCKETS LOGIC
   const [chatOpen, setChatOpen] = useState(false);
@@ -76,13 +79,20 @@ export default function Home() {
   // set up socket connection
   useEffect(() => {
     socketInitializer();
+
+    // Cleanup: Disconnect the socket when the component is unmounted
+    // prevents cases like re-renders leading to "58 people online"
+    return () => {
+      if (socket) {
+        console.log("disconnecting websocket");
+        socket.disconnect();
+      }
+    };
   }, []);
 
   const socketInitializer = () => {
-    // We just call it because we don't need anything else out of it
-    //await fetch("/api/socket");
-
     socket = io("https://pomodoro-server-o0a9.onrender.com");
+    console.log("new socket:", socket);
 
     // when receiving new message, add to message history
     socket.on("newIncomingMessage", (msg) => {
@@ -112,7 +122,7 @@ export default function Home() {
 
   const handleKeypress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     //it triggers by pressing the enter key
-    if (e.keyCode === 13) {
+    if (e.key === "Enter") {
       if (myMessage) {
         sendMessage();
       }
@@ -120,55 +130,79 @@ export default function Home() {
   };
 
   return (
-    <main className="flex w-screen h-screen flex-col items-center justify-center">
+    <main className="flex w-screen h-screen flex-col items-center justify-between p-4">
       {/* TODO: need to have some sort of loading state */}
-      {chatOpen ? (
-        <>
-          <div className="relative flex flex-col justify-end h-[40rem] bg-white min-w-[90%] rounded-xl">
-            <div className="flex flex-row p-4 justify-between ">
-              <div className="flex flex-row space-x-1 items-center">
-                <div className="w-2 h-2 bg-green-600 rounded-full" />
-                <p className="text-xs">{numberOnline} here</p>
-              </div>
-              <div className="text-xs">
-                Chat closes in {minutes}:{secondsPadded}
-              </div>
+      <div className="text-xl font-bold mb-6">pomo.chat</div>
+      {/* {chatOpen ? ( */}
+      {/* <>
+        <div className="relative flex flex-col justify-end h-[40rem] bg-white min-w-[90%] rounded-xl">
+          <div className="flex flex-row p-4 justify-between ">
+            <div className="flex flex-row space-x-1 items-center">
+              <div className="w-2 h-2 bg-green-600 rounded-full" />
+              <p className="text-xs">{numberOnline} here</p>
             </div>
-            <div className="h-full pl-4 last:border-b-0 overflow-y-scroll">
-              {messages.map((msg, i) => {
-                return (
-                  <div className="w-full py-2" key={i}>
-                    {/* note: need random colors for authors */}
-                    <p className="text-slate-500">{msg.author}</p>
-                    <p>{msg.message}</p>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="w-full">
-              <input
-                type="text"
-                placeholder="New message..."
-                value={myMessage}
-                className="outline-none p-4 text-slate-900"
-                onChange={(e) => setMyMessage(e.target.value)}
-                onKeyUp={handleKeypress}
-              />
+            <div className="text-xs">
+              Chat closes in {minutesPadded}:{secondsPadded}
             </div>
           </div>
-        </>
-      ) : (
-        <>
-          <p className="text-xs mb-6">Pomodoro</p>
-          <div className="text-5xl">
-            {minutes}:{secondsPadded}
+          <div className="h-full pl-4 last:border-b-0 overflow-y-scroll">
+            {messages.map((msg, i) => {
+              return (
+                <div className="w-full py-2" key={i}>
+                  <p className="text-slate-500">{msg.author}</p>
+                  <p>{msg.message}</p>
+                </div>
+              );
+            })}
           </div>
-          <div className="flex flex-row space-x-1 items-center mt-6">
-            <div className="w-2 h-2 bg-green-600 rounded-full" />
-            <p className="text-xs">{numberOnline} here</p>
+          <div className="w-full">
+            <input
+              type="text"
+              placeholder="New message..."
+              value={myMessage}
+              className="outline-none p-4 text-slate-900"
+              onChange={(e) => setMyMessage(e.target.value)}
+              onKeyUp={handleKeypress}
+            />
           </div>
-        </>
-      )}
+        </div>
+      </> */}
+      {/* ) : ( */}
+      <>
+        <div className="absolute flex w-full h-full items-center justify-center">
+          {/* <Image
+            src="/images/tomato.png"
+            alt="watercolor illustration of a tomato"
+            width={250}
+            height={250}
+            // fill
+            // sizes="20vw"
+            // style={{
+            //   objectFit: "contain",
+            // }}
+          /> */}
+          <img
+            width="40%"
+            height="auto"
+            src="/images/tomato.png"
+            alt="watercolor illustration of a tomato"
+            style={{ "max-width": "300px" }}
+          />
+        </div>
+        <div className="flex text-[10rem] sm:text-[20rem] flex-col md:flex-row justify-center items-center w-full align-center">
+          <div className="time flex-1 text-center md:text-right pb-10 md:pb-0 md:pr-24">
+            {minutesPadded}
+          </div>
+          <div className="time flex-1 text-center md:text-left pt-10 md:pt-0 md:pl-24">
+            {secondsPadded}
+          </div>
+        </div>
+      </>
+      {/* )} */}
+      <div className="flex flex-row space-x-1 items-center mt-6">
+        <div className="w-2 h-2 bg-green-600 rounded-full" />
+        <p className="text-xs">{numberOnline} here</p>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      cream: "#FAF0DF",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["var(--font-nichrome)", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
