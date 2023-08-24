93f0e87fd80ff03a961bbc4524c50ec8a5b1b9d9
Carolyn Zhang <hi@carolynzhang.com>
2023-08-24 11:07:29 -0700

    testing exported metadata obj

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Head from "next/head";
+import type { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
+import type { Twitter } from "next/dist/lib/metadata/types/twitter-types";
 
 // Fonts
 const nichrome = localFont({
@@ -10,9 +12,28 @@ const nichrome = localFont({
   variable: "--font-nichrome",
 });
 
+const openGraph: OpenGraph = {
+  title: "pomo.chat | social pomodoro timer",
+  description: "25 min focus + 5 min chat",
+  url: "https://pomo.chat",
+  type: "website",
+};
+
+const twitter: Twitter = {
+  card: "summary_large_image",
+  site: "https://pomo.chat",
+  creator: "kelin",
+  description: "25 min focus + 5 min chat",
+};
+
 export const metadata: Metadata = {
-  title: "pomo.chat",
-  description: "25 minutes of focus, 5 minutes of chat",
+  title: "pomo.chat | social pomodoro timer",
+  description: "25 min focus + 5 min chat",
+  keywords:
+    "pomodoro, pomodoro timer, study with me, productivity, study group",
+  creator: "kelin",
+  openGraph: openGraph,
+  twitter: twitter,
 };
 
 export default function RootLayout({
@@ -24,10 +45,11 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <title>🍅 pomo.chat 💬</title>
-        <meta property="og:title" content="pomo.chat | social pomodoro timer" />
+        {/* <meta property="og:title" content="pomo.chat | social pomodoro timer" />
         <meta property="og:description" content="25 min focus + 5 min chat" />
         <meta property="og:url" content="https://pomo.chat" />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" /> */}
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
         {/* note: favicon, link preview images handled by app router */}
         {/* https://nextjs.org/docs/app/api-reference/file-conventions/metadata */}
       </Head>
--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { useEffect, useState, useRef } from "react";
 import Footer from "@/components/Footer";
